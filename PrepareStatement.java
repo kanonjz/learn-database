34ea5d9511839158b69db016728373e63f48cf9b
Kanon <540004716@qq.com>
2017-10-05 17:26:17 +0800

    Update PrepareStatement.java

--- PrepareStatement.java
+++ PrepareStatement.java
@@ -1,10 +1,10 @@
    try {
-			conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-			// 3.执行SQL语句
-			ptmt = conn.prepareStatement("update info set firstname=? where id=?");
-			String name = "雅静";
-			int a = 3;
-			ptmt.setString(1, name);
-			ptmt.setInt(2, a);
-			ptmt.execute();
-		} 
+	conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+	// 3.执行SQL语句
+	ptmt = conn.prepareStatement("update info set firstname=? where id=?");
+	String name = "雅静";
+	int a = 3;
+	ptmt.setString(1, name);
+	ptmt.setInt(2, a);
+	ptmt.execute();
+	} 
